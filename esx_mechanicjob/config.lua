ff3c60916e90e28ae1886ff82aa784be698a1165
Ultra-Meloncrafter <81387462+Ultra-Meloncrafter@users.noreply.github.com>
2021-12-04 00:07:53 +0100

    Add files via upload

---
+++ esx_mechanicjob/config.lua
@@ -0,0 +1,348 @@
+Config                            = {}
+
+Config.DrawDistance               = 100.0
+Config.MarkerType                 = 21
+Config.MarkerSize                 = {x = 1.5, y = 1.5, z = 0.5}
+Config.MarkerColor                = {r = 255, g = 214, b = 0}
+
+Config.EnablePlayerManagement     = true
+Config.EnableArmoryManagement     = true
+Config.EnableJobBlip              = true -- enable blips for cops on duty, requires esx_society
+Config.EnableCustomPeds           = false -- enable custom peds in cloak room? See Config.CustomPeds below to customize peds
+
+Config.EnableESXService           = false -- enable esx service?
+Config.MaxInService               = 5
+
+Config.Locale = 'de'
+
+Config.MechanicStations = {
+
+	LSPD = {
+
+		Blip = {
+		    Coords  = vector3(-339.5680, -157.3945, 38.9985),
+		    Sprite  = 446,
+		    Display = 4,
+		    Scale   = 1.2,
+		    Colour  = 5
+		},
+
+		Cloakrooms = {
+			vector3(-341.4347, -162.0564, 44.5874)
+		},
+
+		Craft = {
+			vector3(-310.7348, -112.9850, 39.015)
+		},
+
+		Garage = {
+			vector3(-314.8016, -123.5802, 39.0156)
+		},
+
+		Armory = {
+			vector3(-352.1116, -171.3611, 39.015)
+		},
+
+		Vehicles = {
+	            {
+			Spawner = vector3(-316.9155, -129.7615, 39.0156),
+			InsideShop = vector3(-361.3735, -92.9842, 44.6641),
+			SpawnPoints = {
+			    {coords = vector3(-320.1255, -129.4653, 37.0156), heading = 72.14, radius = 3.0},
+			    {coords = vector3(-322.3298, -134.1120, 37.0156), heading = 72.14, radius = 3.0},
+	            {coords = vector3(-324.9303, -139.1451, 37.0156), heading = 72.14, radius = 3.0},
+			    {coords = vector3(-326.7224, -144.2958, 37.0156), heading = 72.14, radius = 3.0},
+			 }
+		      }
+		},
+
+		BossActions = {
+            	    vector3(-339.5067, -157.3212, 44.5871)
+		}
+
+	}
+
+}
+
+Config.AuthorizedWeapons = {
+	rekrut = {
+		{weapon = 'WEAPON_PISTOL', components = {0, 0, 1000, 4000}, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	},
+
+	officer = {
+		{weapon = 'WEAPON_PISTOL', components = {0, 0, 1000, 4000, nil}, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	},
+
+        officer2={
+                {weapon = 'WEAPON_PISTOL', components = {0, 0, 1000, 4000, nil}, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+
+	},
+
+	officer3={
+                {weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}	
+
+        },
+           
+
+	lieutenant = {
+		{weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_PUMPSHOTGUN', components = {2000, 6000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	},
+
+        detectiv = {
+		{weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_PUMPSHOTGUN', components = {2000, 6000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	},
+	
+        detectivleitung = {
+		{weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_PUMPSHOTGUN', components = {2000, 6000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	},
+
+	captain = {
+		{weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+		{weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_PUMPSHOTGUN', components = {2000, 6000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}	
+
+	},
+
+        cochief = {
+		{weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_PUMPSHOTGUN', components = {2000, 6000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 1},
+		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	},
+
+	boss = {
+		{weapon = 'WEAPON_COMBATPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_APPISTOL', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_COMBATPDW', components = { 0, 6000, 1000, 4000, 8000, nil }, price = 1},
+                {weapon = 'WEAPON_PUMPSHOTGUN', components = {2000, 6000, nil }, price = 1},
+		{weapon = 'WEAPON_NIGHTSTICK', price = 1},
+		{weapon = 'WEAPON_STUNGUN', price = 0},
+      		{weapon = 'WEAPON_FLASHLIGHT', price = 1}
+	}
+}
+
+Config.AuthorizedVehicles = {
+	car = {
+		recrue = {
+			{model = 'flatbed', price = 1},
+			{model = 'towtruck2', price = 1},
+			{model = 'forklift', price = 1}			
+		},
+
+		novice = {
+			{model = 'flatbed', price = 1},
+			{model = 'towtruck2', price = 1},
+            {model = '17silv', price = 1},
+			{model = 'bmwm4', price = 1},
+			{model = 'honci4', price = 1},
+			{model = 'forklift', price = 1}
+		},
+
+		experimente = {
+			{model = 'flatbed', price = 1},
+			{model = 'towtruck2', price = 1},
+            {model = '17silv', price = 1},
+			{model = 'bmwm4', price = 1},
+			{model = 'honci4', price = 1},
+			{model = 'forklift', price = 1}	
+		},
+
+		chief = {
+			{model = 'flatbed', price = 1},
+			{model = 'towtruck2', price = 1},
+            {model = '17silv', price = 1},
+			{model = 'bmwm4', price = 1},
+			{model = 'honci4', price = 1},
+			{model = 'forklift', price = 1}
+		},														
+
+		boss = {
+			{model = 'flatbed', price = 1},
+			{model = 'towtruck2', price = 1},
+            {model = '17silv', price = 1},
+			{model = 'bmwm4', price = 1},
+			{model = 'honci4', price = 1},
+			{model = 'forklift', price = 1}
+		}
+	},
+}
+
+Config.CustomPeds = {
+	shared = {
+		{label = 'Sheriff Ped', maleModel = 's_m_y_sheriff_01', femaleModel = 's_f_y_sheriff_01'},
+		{label = 'Mechanic Ped', maleModel = 's_m_y_cop_01', femaleModel = 's_f_y_cop_01'}
+	},
+
+	recruit = {},
+
+	officer = {},
+
+	sergeant = {},
+
+	lieutenant = {},
+
+	boss = {}
+}
+
+-- CHECK SKINCHANGER CLIENT MAIN.LUA for matching elements
+Config.Uniforms = {
+	recrue = {
+		male = {
+			tshirt_1 = 130,  tshirt_2 = 1,
+			torso_1 = 55,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 41,
+			pants_1 = 35,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
+			helmet_1 = 46,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 36,  tshirt_2 = 1,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = 45,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	novice = {
+		male = {
+			tshirt_1 = 130,  tshirt_2 = 0,
+			torso_1 = 55,   torso_2 = 0,
+			decals_1 = 8,   decals_2 = 1,
+			arms = 41,
+			pants_1 = 35,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 1,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	experimente = {
+		male = {
+			tshirt_1 = 130,  tshirt_2 = 0,
+			torso_1 = 55,   torso_2 = 0,
+			decals_1 = 8,   decals_2 = 1,
+			arms = 41,
+			pants_1 = 35,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 1,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	chief = {
+		male = {
+			tshirt_1 = 130,  tshirt_2 = 0,
+			torso_1 = 55,   torso_2 = 0,
+			decals_1 = 8,   decals_2 = 1,
+			arms = 41,
+			pants_1 = 35,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 1,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	},
+
+	boss = {
+		male = {
+			tshirt_1 = 130,  tshirt_2 = 0,
+			torso_1 = 55,   torso_2 = 0,
+			decals_1 = 8,   decals_2 = 3,
+			arms = 41,
+			pants_1 = 35,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		},
+		female = {
+			tshirt_1 = 35,  tshirt_2 = 0,
+			torso_1 = 48,   torso_2 = 0,
+			decals_1 = 7,   decals_2 = 3,
+			arms = 44,
+			pants_1 = 34,   pants_2 = 0,
+			shoes_1 = 27,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
+			chain_1 = 0,    chain_2 = 0,
+			ears_1 = 2,     ears_2 = 0
+		}
+	}
+}
---
+++ esx_mechanicjob/server/main.lua
@@ -0,0 +1,502 @@
+ESX = nil
+PlayersHarvesting  = {}
+PlayersHarvesting2 = {}
+PlayersHarvesting3 = {}
+PlayersCrafting    = {}
+PlayersCrafting2   = {}
+PlayersCrafting3   = {}
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+if Config.EnableESXService then
+	TriggerEvent('esx_service:activateService', 'mechanic', Config.MaxInService)
+end
+
+TriggerEvent('esx_phone:registerNumber', 'mechanic', _U('alert_mechanic'), true, true)
+TriggerEvent('esx_society:registerSociety', 'mechanic', 'Mechanic', 'society_mechanic', 'society_mechanic', 'society_mechanic', {type = 'public'})
+
+local function Harvest(source)
+	SetTimeout(4000, function()
+
+		if PlayersHarvesting[source] == true then
+			local xPlayer = ESX.GetPlayerFromId(source)
+			local GazBottleQuantity = xPlayer.getInventoryItem('gazbottle').count
+
+			if GazBottleQuantity >= 5 then
+				TriggerClientEvent('esx:showNotification', source, _U('you_do_not_room'))
+			else
+				xPlayer.addInventoryItem('gazbottle', 1)
+				Harvest(source)
+			end
+		end
+
+	end)
+end
+
+RegisterServerEvent('esx_mechanicjob:startHarvest')
+AddEventHandler('esx_mechanicjob:startHarvest', function()
+	local _source = source
+	PlayersHarvesting[_source] = true
+	TriggerClientEvent('esx:showNotification', _source, _U('recovery_gas_can'))
+	Harvest(source)
+end)
+
+RegisterServerEvent('esx_mechanicjob:stopHarvest')
+AddEventHandler('esx_mechanicjob:stopHarvest', function()
+	local _source = source
+	PlayersHarvesting[_source] = false
+end)
+
+local function Harvest2(source)
+	SetTimeout(4000, function()
+
+		if PlayersHarvesting2[source] == true then
+			local xPlayer = ESX.GetPlayerFromId(source)
+			local FixToolQuantity = xPlayer.getInventoryItem('fixtool').count
+
+			if FixToolQuantity >= 5 then
+				TriggerClientEvent('esx:showNotification', source, _U('you_do_not_room'))
+			else
+				xPlayer.addInventoryItem('fixtool', 1)
+				Harvest2(source)
+			end
+		end
+
+	end)
+end
+
+RegisterServerEvent('esx_mechanicjob:startHarvest2')
+AddEventHandler('esx_mechanicjob:startHarvest2', function()
+	local _source = source
+	PlayersHarvesting2[_source] = true
+	TriggerClientEvent('esx:showNotification', _source, _U('recovery_repair_tools'))
+	Harvest2(_source)
+end)
+
+RegisterServerEvent('esx_mechanicjob:stopHarvest2')
+AddEventHandler('esx_mechanicjob:stopHarvest2', function()
+	local _source = source
+	PlayersHarvesting2[_source] = false
+end)
+
+local function Harvest3(source)
+	SetTimeout(4000, function()
+
+		if PlayersHarvesting3[source] == true then
+			local xPlayer = ESX.GetPlayerFromId(source)
+			local CaroToolQuantity = xPlayer.getInventoryItem('carotool').count
+			if CaroToolQuantity >= 5 then
+				TriggerClientEvent('esx:showNotification', source, _U('you_do_not_room'))
+			else
+				xPlayer.addInventoryItem('carotool', 1)
+				Harvest3(source)
+			end
+		end
+
+	end)
+end
+
+RegisterServerEvent('esx_mechanicjob:startHarvest3')
+AddEventHandler('esx_mechanicjob:startHarvest3', function()
+	local _source = source
+	PlayersHarvesting3[_source] = true
+	TriggerClientEvent('esx:showNotification', _source, _U('recovery_body_tools'))
+	Harvest3(_source)
+end)
+
+RegisterServerEvent('esx_mechanicjob:stopHarvest3')
+AddEventHandler('esx_mechanicjob:stopHarvest3', function()
+	local _source = source
+	PlayersHarvesting3[_source] = false
+end)
+
+local function Craft(source)
+	SetTimeout(4000, function()
+
+		if PlayersCrafting[source] == true then
+			local xPlayer = ESX.GetPlayerFromId(source)
+			local GazBottleQuantity = xPlayer.getInventoryItem('gazbottle').count
+
+			if GazBottleQuantity <= 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_gas_can'))
+			else
+				xPlayer.removeInventoryItem('gazbottle', 1)
+				xPlayer.addInventoryItem('blowpipe', 1)
+				Craft(source)
+			end
+		end
+
+	end)
+end
+
+RegisterServerEvent('esx_mechanicjob:startCraft')
+AddEventHandler('esx_mechanicjob:startCraft', function()
+	local _source = source
+	PlayersCrafting[_source] = true
+	TriggerClientEvent('esx:showNotification', _source, _U('assembling_blowtorch'))
+	Craft(_source)
+end)
+
+RegisterServerEvent('esx_mechanicjob:stopCraft')
+AddEventHandler('esx_mechanicjob:stopCraft', function()
+	local _source = source
+	PlayersCrafting[_source] = false
+end)
+
+local function Craft2(source)
+	SetTimeout(4000, function()
+
+		if PlayersCrafting2[source] == true then
+			local xPlayer = ESX.GetPlayerFromId(source)
+			local FixToolQuantity = xPlayer.getInventoryItem('fixtool').count
+
+			if FixToolQuantity <= 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_repair_tools'))
+			else
+				xPlayer.removeInventoryItem('fixtool', 1)
+				xPlayer.addInventoryItem('fixkit', 1)
+				Craft2(source)
+			end
+		end
+
+	end)
+end
+
+RegisterServerEvent('esx_mechanicjob:startCraft2')
+AddEventHandler('esx_mechanicjob:startCraft2', function()
+	local _source = source
+	PlayersCrafting2[_source] = true
+	TriggerClientEvent('esx:showNotification', _source, _U('assembling_repair_kit'))
+	Craft2(_source)
+end)
+
+RegisterServerEvent('esx_mechanicjob:stopCraft2')
+AddEventHandler('esx_mechanicjob:stopCraft2', function()
+	local _source = source
+	PlayersCrafting2[_source] = false
+end)
+
+local function Craft3(source)
+	SetTimeout(4000, function()
+
+		if PlayersCrafting3[source] == true then
+			local xPlayer = ESX.GetPlayerFromId(source)
+			local CaroToolQuantity = xPlayer.getInventoryItem('carotool').count
+
+			if CaroToolQuantity <= 0 then
+				TriggerClientEvent('esx:showNotification', source, _U('not_enough_body_tools'))
+			else
+				xPlayer.removeInventoryItem('carotool', 1)
+				xPlayer.addInventoryItem('carokit', 1)
+				Craft3(source)
+			end
+		end
+
+	end)
+end
+
+RegisterServerEvent('esx_mechanicjob:startCraft3')
+AddEventHandler('esx_mechanicjob:startCraft3', function()
+	local _source = source
+	PlayersCrafting3[_source] = true
+	TriggerClientEvent('esx:showNotification', _source, _U('assembling_body_kit'))
+	Craft3(_source)
+end)
+
+RegisterServerEvent('esx_mechanicjob:stopCraft3')
+AddEventHandler('esx_mechanicjob:stopCraft3', function()
+	local _source = source
+	PlayersCrafting3[_source] = false
+end)
+
+RegisterNetEvent('esx_mechanicjob:getStockItem')
+AddEventHandler('esx_mechanicjob:getStockItem', function(itemName, count)
+	local _source = source
+	local xPlayer = ESX.GetPlayerFromId(_source)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_mechanic', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- is there enough in the society?
+		if count > 0 and inventoryItem.count >= count then
+
+			inventory.removeItem(itemName, count)
+			xPlayer.addInventoryItem(itemName, count)
+			xPlayer.showNotification(_U('have_withdrawn', count, inventoryItem.label))
+		else
+			xPlayer.showNotification(_U('quantity_invalid'))
+		end
+	end)
+end)
+
+RegisterNetEvent('esx_mechanicjob:putStockItems')
+AddEventHandler('esx_mechanicjob:putStockItems', function(itemName, count)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local sourceItem = xPlayer.getInventoryItem(itemName)
+
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_mechanic', function(inventory)
+		local inventoryItem = inventory.getItem(itemName)
+
+		-- does the player have enough of the item?
+		if sourceItem.count >= count and count > 0 then
+			xPlayer.removeInventoryItem(itemName, count)
+			inventory.addItem(itemName, count)
+			xPlayer.showNotification(_U('have_deposited', count, inventoryItem.label))
+		else
+			xPlayer.showNotification(_U('quantity_invalid'))
+		end
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:getArmoryWeapons', function(source, cb)
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_mechanic', function(store)
+		local weapons = store.get('weapons')
+
+		if weapons == nil then
+			weapons = {}
+		end
+
+		cb(weapons)
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:addArmoryWeapon', function(source, cb, weaponName, removeWeapon)
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if removeWeapon then
+		xPlayer.removeWeapon(weaponName)
+	end
+
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_mechanic', function(store)
+		local weapons = store.get('weapons') or {}
+		local foundWeapon = false
+
+		for i=1, #weapons, 1 do
+			if weapons[i].name == weaponName then
+				weapons[i].count = weapons[i].count + 1
+				foundWeapon = true
+				break
+			end
+		end
+
+		if not foundWeapon then
+			table.insert(weapons, {
+				name  = weaponName,
+				count = 1
+			})
+		end
+
+		store.set('weapons', weapons)
+		cb()
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:removeArmoryWeapon', function(source, cb, weaponName)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	xPlayer.addWeapon(weaponName, 500)
+
+	TriggerEvent('esx_datastore:getSharedDataStore', 'society_mechanic', function(store)
+		local weapons = store.get('weapons') or {}
+
+		local foundWeapon = false
+
+		for i=1, #weapons, 1 do
+			if weapons[i].name == weaponName then
+				weapons[i].count = (weapons[i].count > 0 and weapons[i].count - 1 or 0)
+				foundWeapon = true
+				break
+			end
+		end
+
+		if not foundWeapon then
+			table.insert(weapons, {
+				name = weaponName,
+				count = 0
+			})
+		end
+
+		store.set('weapons', weapons)
+		cb()
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:buyWeapon', function(source, cb, weaponName, type, componentNum)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local authorizedWeapons, selectedWeapon = Config.AuthorizedWeapons[xPlayer.job.grade_name]
+
+	for k,v in ipairs(authorizedWeapons) do
+		if v.weapon == weaponName then
+			selectedWeapon = v
+			break
+		end
+	end
+
+	if not selectedWeapon then
+		print(('esx_mechanicjob: %s attempted to buy an invalid weapon.'):format(xPlayer.identifier))
+		cb(false)
+	else
+		-- Weapon
+		if type == 1 then
+			if xPlayer.getMoney() >= selectedWeapon.price then
+				xPlayer.removeMoney(selectedWeapon.price)
+				xPlayer.addWeapon(weaponName, 100)
+
+				cb(true)
+			else
+				cb(false)
+			end
+
+		-- Weapon Component
+		elseif type == 2 then
+			local price = selectedWeapon.components[componentNum]
+			local weaponNum, weapon = ESX.GetWeapon(weaponName)
+			local component = weapon.components[componentNum]
+
+			if component then
+				if xPlayer.getMoney() >= price then
+					xPlayer.removeMoney(price)
+					xPlayer.addWeaponComponent(weaponName, component.name)
+
+					cb(true)
+				else
+					cb(false)
+				end
+			else
+				print(('esx_mechanicjob: %s attempted to buy an invalid weapon component.'):format(xPlayer.identifier))
+				cb(false)
+			end
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:buyJobVehicle', function(source, cb, vehicleProps, type)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local price = getPriceFromHash(vehicleProps.model, xPlayer.job.grade_name, type)
+
+	-- vehicle model not found
+	if price == 0 then
+		print(('esx_mechanicjob: %s attempted to exploit the shop! (invalid vehicle model)'):format(xPlayer.identifier))
+		cb(false)
+	else
+		if xPlayer.getMoney() >= price then
+			xPlayer.removeMoney(price)
+
+			MySQL.Async.execute('INSERT INTO owned_vehicles (owner, vehicle, plate, type, job, `stored`) VALUES (@owner, @vehicle, @plate, @type, @job, @stored)', {
+				['@owner'] = xPlayer.identifier,
+				['@vehicle'] = json.encode(vehicleProps),
+				['@plate'] = vehicleProps.plate,
+				['@type'] = type,
+				['@job'] = xPlayer.job.name,
+				['@stored'] = true
+			}, function (rowsChanged)
+				cb(true)
+			end)
+		else
+			cb(false)
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:storeNearbyVehicle', function(source, cb, nearbyVehicles)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local foundPlate, foundNum
+
+	for k,v in ipairs(nearbyVehicles) do
+		local result = MySQL.Sync.fetchAll('SELECT plate FROM owned_vehicles WHERE owner = @owner AND plate = @plate AND job = @job', {
+			['@owner'] = xPlayer.identifier,
+			['@plate'] = v.plate,
+			['@job'] = xPlayer.job.name
+		})
+
+		if result[1] then
+			foundPlate, foundNum = result[1].plate, k
+			break
+		end
+	end
+
+	if not foundPlate then
+		cb(false)
+	else
+		MySQL.Async.execute('UPDATE owned_vehicles SET `stored` = true WHERE owner = @owner AND plate = @plate AND job = @job', {
+			['@owner'] = xPlayer.identifier,
+			['@plate'] = foundPlate,
+			['@job'] = xPlayer.job.name
+		}, function (rowsChanged)
+			if rowsChanged == 0 then
+				print(('esx_mechanicjob: %s has exploited the garage!'):format(xPlayer.identifier))
+				cb(false)
+			else
+				cb(true, foundNum)
+			end
+		end)
+	end
+end)
+
+function getPriceFromHash(vehicleHash, jobGrade, type)
+	local vehicles = Config.AuthorizedVehicles[type][jobGrade]
+
+	for k,v in ipairs(vehicles) do
+		if GetHashKey(v.model) == vehicleHash then
+			return v.price
+		end
+	end
+
+	return 0
+end
+
+ESX.RegisterServerCallback('esx_mechanicjob:getStockItems', function(source, cb)
+	TriggerEvent('esx_addoninventory:getSharedInventory', 'society_mechanic', function(inventory)
+		cb(inventory.items)
+	end)
+end)
+
+ESX.RegisterServerCallback('esx_mechanicjob:getPlayerInventory', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local items   = xPlayer.inventory
+
+	cb({items = items})
+end)
+
+AddEventHandler('playerDropped', function()
+	-- Save the source in case we lose it (which happens a lot)
+	local playerId = source
+
+	-- Did the player ever join?
+	if playerId then
+		local xPlayer = ESX.GetPlayerFromId(playerId)
+
+		-- Is it worth telling all clients to refresh?
+		if xPlayer and xPlayer.job.name == 'mechanic' then
+			Citizen.Wait(5000)
+			TriggerClientEvent('esx_mechanicjob:updateBlip', -1)
+		end
+	end
+end)
+
+RegisterNetEvent('esx_mechanicjob:spawned')
+AddEventHandler('esx_mechanicjob:spawned', function()
+	local xPlayer = ESX.GetPlayerFromId(playerId)
+
+	if xPlayer and xPlayer.job.name == 'mechanic' then
+		Citizen.Wait(5000)
+		TriggerClientEvent('esx_mechanicjob:updateBlip', -1)
+	end
+end)
+
+RegisterNetEvent('esx_mechanicjob:forceBlip')
+AddEventHandler('esx_mechanicjob:forceBlip', function()
+	TriggerClientEvent('esx_mechanicjob:updateBlip', -1)
+end)
+
+AddEventHandler('onResourceStart', function(resource)
+	if resource == GetCurrentResourceName() then
+		Citizen.Wait(5000)
+		TriggerClientEvent('esx_mechanicjob:updateBlip', -1)
+	end
+end)
+
+AddEventHandler('onResourceStop', function(resource)
+	if resource == GetCurrentResourceName() then
+		TriggerEvent('esx_phone:removeNumber', 'mechanic')
+	end
+end)
