2927a8dc317dfbdf3cb10d027839d49eb3ef6447
Ultra-Meloncrafter <81387462+Ultra-Meloncrafter@users.noreply.github.com>
2021-12-04 14:18:51 +0100

    Update config.lua

--- esx_mechanicjob/config.lua
+++ esx_mechanicjob/config.lua
@@ -119,126 +119,127 @@ Config.CustomPeds = {
 Config.Uniforms = {
 	recrue = {
 		male = {
-			tshirt_1 = 130,  tshirt_2 = 1,
-			torso_1 = 55,   torso_2 = 0,
+			tshirt_1 = 15,  tshirt_2 = 1,
+			torso_1 = 248,   torso_2 = 13,
 			decals_1 = 0,   decals_2 = 0,
-			arms = 41,
-			pants_1 = 35,   pants_2 = 0,
+			arms = 96,
+			pants_1 = 9,   pants_2 = 7,
 			shoes_1 = 25,   shoes_2 = 0,
-			helmet_1 = 46,  helmet_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		},
 		female = {
-			tshirt_1 = 36,  tshirt_2 = 1,
-			torso_1 = 48,   torso_2 = 0,
-			decals_1 = 0,   decals_2 = 0,
-			arms = 44,
-			pants_1 = 34,   pants_2 = 0,
-			shoes_1 = 27,   shoes_2 = 0,
-			helmet_1 = 45,  helmet_2 = 0,
+			tshirt_1 = 14,  tshirt_2 = 0,
+			torso_1 = 256,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 1,
+			arms = 111,     
+			pants_1 = 32,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
+			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		}
 	},
 
 	novice = {
 		male = {
-			tshirt_1 = 130,  tshirt_2 = 0,
-			torso_1 = 55,   torso_2 = 0,
-			decals_1 = 8,   decals_2 = 1,
-			arms = 41,
-			pants_1 = 35,   pants_2 = 0,
+			tshirt_1 = 15,  tshirt_2 = 1,
+			torso_1 = 248,   torso_2 = 13,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 96,
+			pants_1 = 9,   pants_2 = 7,
 			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		},
 		female = {
-			tshirt_1 = 35,  tshirt_2 = 0,
-			torso_1 = 48,   torso_2 = 0,
-			decals_1 = 7,   decals_2 = 1,
-			arms = 44,
-			pants_1 = 34,   pants_2 = 0,
-			shoes_1 = 27,   shoes_2 = 0,
+			tshirt_1 = 14,  tshirt_2 = 0,
+			torso_1 = 256,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 1,
+			arms = 111,     
+			pants_1 = 32,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		}
 	},
 
 	experimente = {
 		male = {
-			tshirt_1 = 130,  tshirt_2 = 0,
-			torso_1 = 55,   torso_2 = 0,
-			decals_1 = 8,   decals_2 = 1,
-			arms = 41,
-			pants_1 = 35,   pants_2 = 0,
+			tshirt_1 = 15,  tshirt_2 = 1,
+			torso_1 = 248,   torso_2 = 13,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 96,
+			pants_1 = 9,   pants_2 = 7,
 			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		},
 		female = {
-			tshirt_1 = 35,  tshirt_2 = 0,
-			torso_1 = 48,   torso_2 = 0,
-			decals_1 = 7,   decals_2 = 1,
-			arms = 44,
-			pants_1 = 34,   pants_2 = 0,
-			shoes_1 = 27,   shoes_2 = 0,
+			tshirt_1 = 14,  tshirt_2 = 0,
+			torso_1 = 256,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 1,
+			arms = 111,     
+			pants_1 = 32,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		}
 	},
 
 	chief = {
 		male = {
-			tshirt_1 = 130,  tshirt_2 = 0,
-			torso_1 = 55,   torso_2 = 0,
-			decals_1 = 8,   decals_2 = 1,
-			arms = 41,
-			pants_1 = 35,   pants_2 = 0,
+			tshirt_1 = 15,  tshirt_2 = 1,
+			torso_1 = 248,   torso_2 = 13,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 96,
+			pants_1 = 9,   pants_2 = 7,
 			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		},
 		female = {
-			tshirt_1 = 35,  tshirt_2 = 0,
-			torso_1 = 48,   torso_2 = 0,
-			decals_1 = 7,   decals_2 = 1,
-			arms = 44,
-			pants_1 = 34,   pants_2 = 0,
-			shoes_1 = 27,   shoes_2 = 0,
+			tshirt_1 = 14,  tshirt_2 = 0,
+			torso_1 = 256,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 1,
+			arms = 111,     
+			pants_1 = 32,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		}
 	},
 
 	boss = {
 		male = {
-			tshirt_1 = 130,  tshirt_2 = 0,
-			torso_1 = 55,   torso_2 = 0,
-			decals_1 = 8,   decals_2 = 3,
-			arms = 41,
-			pants_1 = 35,   pants_2 = 0,
+			tshirt_1 = 15,  tshirt_2 = 1,
+			torso_1 = 248,   torso_2 = 13,
+			decals_1 = 0,   decals_2 = 0,
+			arms = 96,
+			pants_1 = 9,   pants_2 = 7,
 			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		},
 		female = {
-			tshirt_1 = 35,  tshirt_2 = 0,
-			torso_1 = 48,   torso_2 = 0,
-			decals_1 = 7,   decals_2 = 3,
-			arms = 44,
-			pants_1 = 34,   pants_2 = 0,
-			shoes_1 = 27,   shoes_2 = 0,
+			tshirt_1 = 14,  tshirt_2 = 0,
+			torso_1 = 256,   torso_2 = 0,
+			decals_1 = 0,   decals_2 = 1,
+			arms = 116,     arms_2 = 10,
+			pants_1 = 32,   pants_2 = 0,
+			shoes_1 = 25,   shoes_2 = 0,
 			helmet_1 = -1,  helmet_2 = 0,
 			chain_1 = 0,    chain_2 = 0,
-			ears_1 = 2,     ears_2 = 0
+			ears_1 = 1,     ears_2 = 0
 		}
 	}
 }
+
